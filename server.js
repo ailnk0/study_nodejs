967d9b5b9bcd1056f753073f8d5046964aeb13f1
Benedict <ailnk@naver.com>
2023-05-06 21:43:14 +0900

    chore: npm install body-parser
    - ready for post with parsing request
    - add code to use body-parser

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.listen(8080, () => {
   console.log("Server running on port 8080");
