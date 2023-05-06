265a34cf13899104557f172745de634d3142ff6e
Benedict <ailnk@naver.com>
2023-05-06 21:33:09 +0900

    feat: your first server
    - if someone visit to /pet, then popup dialog related to pet
    - node server.js
    - localhost:8080/pet

--- server.js
+++ server.js
@@ -4,3 +4,9 @@ const app = express();
 app.listen(8080, () => {
   console.log("Server running on port 8080");
 });
+
+// if someone visit to /pet,
+// then popup dialog related to pet
+app.get("/pet", (req, res) => {
+  res.send("This is pet page");
+});
