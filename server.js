2d9caae11f849f525bcefd30d48a84e62f88c002
Benedict <ailnk@naver.com>
2023-05-06 22:00:51 +0900

    chore: npm install mongodb
    - install mongodb
    - https://www.mongodb.com/atlas/database
    - add code to init mongodb

--- server.js
+++ server.js
@@ -3,6 +3,33 @@ const app = express();
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: true }));
 
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri =
+  "mongodb+srv://ailnk0:p52OD5ZpiEJdq412@cluster0.86dc03b.mongodb.net/?retryWrites=true&w=majority";
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
 app.listen(8080, () => {
   console.log("Server running on port 8080");
 });
